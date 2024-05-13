daac3e880e2a8c8ab194e393b5d0f64598856967
Nadine Han <nhan@holylogin03.rc.fas.harvard.edu>
2024-05-12 23:51:38 -0400

    add new model files

--- AnimalPathMapping/AnimalPathMapping/models/detectron2/run_train_val_custom_data_v2.sh
+++ AnimalPathMapping/AnimalPathMapping/models/detectron2/run_train_val_custom_data_v2.sh
@@ -9,18 +9,32 @@
 #SBATCH -o bash-outputs/%A-%a.out  # File to which STDOUT will be written, %A inserts jobid %a inserts array id
 #SBATCH -e bash-errors/%A-%a.err  # File to which STDERR will be written, %A inserts jobid %a inserts array id
 
+## Commented out to work on Sam's account
+# source /n/sw/eb/apps/centos7/Anaconda3/2020.11/etc/profile.d/conda.sh
+# conda deactivate
+# conda activate /n/davies_lab/Lab/shared_conda_envs/detectron_env/
+
+# module load gcc/13.2.0-fasrc01
+
+## Added (replaced lines commented out above) to work on Sam's account
 source /n/sw/eb/apps/centos7/Anaconda3/2020.11/etc/profile.d/conda.sh
+
 conda deactivate
 conda activate /n/davies_lab/Lab/shared_conda_envs/detectron_env/
 
 module load gcc/13.2.0-fasrc01
 
-## arguments for python script, see python script called for thier specifications
-img_dir="firestorm-3/image-tiles/all-tiles-400px/rgb-tiles/png-images"
-mask_dir="firestorm-3/image-tiles/all-tiles-400px/path-masks-finished"
-output_dir="/n/davies_lab/Lab/shared_projects/AnimalPathMapping/train_val_outputs/train_val_400px_10k_iter_output"
+## arguments for python script, see python script called for their specifications
+img_dir="/n/davies_lab/Lab/shared_projects/AnimalPathMapping/firestorm-3/image-tiles/all-tiles-800px/rgb-tiles/png-images"
+mask_dir="/n/davies_lab/Lab/shared_projects/AnimalPathMapping/firestorm-3/image-tiles/all-tiles-800px/path-masks-finished"
+output_dir="/n/davies_lab/Lab/shared_projects/AnimalPathMapping/train_val_outputs/train_val_800px_davies_lbls_output"
 
-set -x
-date
-source ~/.bashrc
-python ./train_val_custom_data_v2.py $img_dir $mask_dir $output_dir
+## Commented out to work on Sam's account
+# set -x
+# date
+# source ~/.bashrc
+## python ./train_val_custom_data_v2.py $img_dir $mask_dir $output_dir
+python /n/davies_lab/Lab/shared_projects/AnimalPathMapping/cs288-animal-paths/AnimalPathMapping/AnimalPathMapping/models/detectron2/train_val_custom_data_v2.py $img_dir $mask_dir $output_dir
+
+## To run this, make sure your current working directory is the outermost directory of this project.
+## Enter 'sbatch run_train_val_custom_data_v2.sh' in the terminal
